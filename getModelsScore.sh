6d8181283d438c04485c8bb4964aa687fb45ff10
SAUGER Gabriel <gabriel.sauger@loria.fr>
2025-04-18 18:11:17 +0200

    sub update

--- getModelsScore.sh
+++ getModelsScore.sh
@@ -1,7 +1,6 @@
-model="$1"
-LOG=$(realpath "$2")
-OUT_DIR=$(realpath "$3")
+OUT_DIR=$(realpath "$1")
 
+LOG="./models.log"
 CISCO_MODELS=$(realpath "Models/")
 CISCO_RESULTS=$(realpath "Results/")
 mkdir -p $CISCO_RESULTS/csv/
@@ -9,101 +8,95 @@ rm -r $CISCO_RESULTS/csv/*
 mkdir -p $OUT_DIR
 CWD=$(pwd)
 
-if [ "$model" != "a2v" -a "$model" != "gmn"  -a "$model" != "all" ]; then
-	echo "error, "$model" model not recognized"
-	notif error "$0" "getModelScores.sh: model not recognized"
+cd $CISCO_MODELS/SAFE/
+echo "Running SAFE test_script in $(pwd)" >> $LOG
+if ! ./test_script.sh; then
+	echo "Error running test script, no Dataset testing results created"
+	echo "Error running test script, no Dataset testing results created" >> $LOG
+	notif error "$0 $*" finished
 	exit 1
 fi
+echo "Done" >> $LOG
 
-if [ "$model" = "gmn" ]; then
-	cd $CISCO_MODELS/GGSNN-GMN/
-	echo "Running gmn test_script in $(pwd)" >> $LOG
-	if ! ./test_script.sh gmn_opc; then
-		cd -
-		echo "Error running test script, no Dataset testing results created"
-		echo "Error running test script, no Dataset testing results created" >> $LOG
-		exit 1
-	fi
-	if ! cp $CISCO_RESULTS/csv/pairs_results_Dataset-Muaz_gmn_opc.csv $OUT_DIR/; then
-		cd -
-		echo "error copying result csv"
-		echo "error copying result csv" >> $LOG
-		exit 1
-	fi
+cd $CISCO_MODELS/Asm2vec/
+echo "Running asm2vec test_script in $(pwd)" >> $LOG
+if ! ./test_script.sh; then
+	echo "Error running test script, no Dataset testing results created"
+	echo "Error running test script, no Dataset testing results created" >> $LOG
+	notif error "$0 $*" finished
+	exit 1
+fi
+echo "Done" >> $LOG
 
-elif [ "$model" = "a2v" ]; then
-	cd $CISCO_MODELS/Asm2vec/
-	echo "Running asm2vec test_script in $(pwd)" >> $LOG
-	if ! ./test_script.sh; then
-		echo "Error running test script, no Dataset testing results created"
-		echo "Error running test script, no Dataset testing results created" >> $LOG
-		exit 1
-	fi
-	if ! cp $CISCO_RESULTS/csv/pairs_results_Dataset-Muaz_a2v.csv $OUT_DIR/; then
-		echo "error copying result csv"
-		echo "error copying result csv" >> $LOG
-		exit 1
-	fi
+cd $CISCO_MODELS/GGSNN-GMN/
+echo "Running gnn_opc test_script in $(pwd)" >> $LOG
+if ! ./test_script.sh gnn_opc; then
+	echo "Error running test script, no Dataset testing results created"
+	echo "Error running test script, no Dataset testing results created" >> $LOG
+	notif error "$0 $*" finished
+	exit 1
+fi
+echo "Done" >> $LOG
 
-elif [ $model = "all" ]; then
+echo "Running gmn test_script in $(pwd)" >> $LOG
+if ! ./test_script.sh gmn_opc; then
+	echo "Error running test script, no Dataset testing results created"
+	echo "Error running test script, no Dataset testing results created" >> $LOG
+	notif error "$0 $*" finished
+	exit 1
+fi
+echo "Done" >> $LOG
 
-	cd $CISCO_MODELS/SAFE/
-	echo "Running SAFE test_script in $(pwd)" >> $LOG
-	if ! ./test_script.sh; then
-		echo "Error running test script, no Dataset testing results created"
-		echo "Error running test script, no Dataset testing results created" >> $LOG
-		notif error "$0 $*" finished
-		exit 1
-	fi
-	echo "Done" >> $LOG
+cd $CISCO_MODELS/Zeek/
+echo "Running gnn test_script in $(pwd)" >> $LOG
+if ! ./test_script.sh; then
+	echo "Error running test script, no Dataset testing results created"
+	echo "Error running test script, no Dataset testing results created" >> $LOG
+	notif error "$0 $*" finished
+	exit 1
+fi
+echo "Done" >> $LOG
 
-	cd $CISCO_MODELS/Asm2vec/
-	echo "Running asm2vec test_script in $(pwd)" >> $LOG
-	if ! ./test_script.sh; then
-		echo "Error running test script, no Dataset testing results created"
-		echo "Error running test script, no Dataset testing results created" >> $LOG
-		notif error "$0 $*" finished
-		exit 1
-	fi
-	echo "Done" >> $LOG
+cd $CISCO_MODELS/jTrans/
+conda activate jtrans
+echo "Running jTrans test_script in $(pwd)" >> $LOG
+if ! ./test_script.sh; then
+	echo "Error running test script, no Dataset testing results created"
+	echo "Error running test script, no Dataset testing results created" >> $LOG
+	notif error "$0 $*" finished
+	exit 1
+fi
+echo "Done" >> $LOG
 
-	cd $CISCO_MODELS/GGSNN-GMN/
-	echo "Running gnn_opc test_script in $(pwd)" >> $LOG
-	if ! ./test_script.sh gnn_opc; then
-		echo "Error running test script, no Dataset testing results created"
-		echo "Error running test script, no Dataset testing results created" >> $LOG
-		notif error "$0 $*" finished
-		exit 1
-	fi
-	echo "Done" >> $LOG
+cd $CISCO_MODELS/HermesSim/
+conda activate hermessim
+echo "Running HermesSim test_script in $(pwd)" >> $LOG
+if ! ./test_script.sh; then
+	echo "Error running test script, no Dataset testing results created"
+	echo "Error running test script, no Dataset testing results created" >> $LOG
+	notif error "$0 $*" finished
+	exit 1
+fi
+echo "Done" >> $LOG
 
-	echo "Running gmn test_script in $(pwd)" >> $LOG
-	if ! ./test_script.sh gmn_opc; then
-		echo "Error running test script, no Dataset testing results created"
-		echo "Error running test script, no Dataset testing results created" >> $LOG
-		notif error "$0 $*" finished
-		exit 1
-	fi
-	echo "Done" >> $LOG
+cd $CISCO_MODELS/Trex/
+conda activate jtrans
+echo "Running Trex test_script in $(pwd)" >> $LOG
+if ! ./test_script.sh; then
+	echo "Error running test script, no Dataset testing results created"
+	echo "Error running test script, no Dataset testing results created" >> $LOG
+	notif error "$0 $*" finished
+	exit 1
+fi
+echo "Done" >> $LOG
 
-	cd $CISCO_MODELS/Zeek/
-	echo "Running gnn test_script in $(pwd)" >> $LOG
-	if ! ./test_script.sh; then
-		echo "Error running test script, no Dataset testing results created"
-		echo "Error running test script, no Dataset testing results created" >> $LOG
-		notif error "$0 $*" finished
-		exit 1
-	fi
-	echo "Done" >> $LOG
+cd $CWD
 
-	cd $CWD
-
-	# copy the resulting csvs
-	if ! cp $CISCO_RESULTS/csv/pairs_results_Dataset-Muaz_*.csv $OUT_DIR/; then
-		echo "error copying result csvs"
-		echo "error copying result csvs" >> $LOG
-		exit 1
-	fi
+# copy the resulting csvs
+if ! cp $CISCO_RESULTS/csv/pairs_results_Dataset-Muaz_*.csv $OUT_DIR/; then
+	echo "error copying result csvs"
+	echo "error copying result csvs" >> $LOG
+	exit 1
 fi
 
 echo "Done"
